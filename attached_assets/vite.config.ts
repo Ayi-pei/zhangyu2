c5a2df01ba71f4c99e3d36a40c2ead48f8124977
阿义 <ayi198701@gmail.com>
2025-02-11 05:13:41 +0000

    点击同步

--- attached_assets/vite.config.ts
+++ attached_assets/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     middlewareMode: false,
-    port: 5173, // 可以修改端口
+    port: 3000, // 可以修改端口
   },
   build: {
     rollupOptions: {
-      input: './attached_assets/index.html', // 确保这是正确的路径
+      input: "./attached_assets/index.html", // 确保这是正确的路径
     },
   },
 });
