af5616a9c8e96b7fd4154bb9d3501b66ee11616a
阿义 <ayi198701@gmail.com>
2025-02-14 00:38:19 +0800

    - 更新文件

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
-import PlayerInfo from './components/PlayerInfo';
-import BindingForm from './components/BindingForm';
-import ExchangeForm from './components/ExchangeForm';
-import SupportDialog from './components/SupportDialog';
+import PlayerInfo from '../components/PlayerInfo';
+import BindingForm from '../components/BindingForm';
+import ExchangeForm from '../components/ExchangeForm';
+import SupportDialog from '../components/SupportDialog';
 
 const Profile = () => {
   const [showSupportDialog, setShowSupportDialog] = useState(false);
+
+  // 绑定银行卡信息
+  const [bindingCardNumber, setBindingCardNumber] = useState('');
+  const [bindingBank, setBindingBank] = useState('');
+
+  // **补全缺少的状态**
+  const [bindingExchangeCode, setBindingExchangeCode] = useState('');
+  const [bindingCardHolder, setBindingCardHolder] = useState('');
 
   const playerStats = {
     balance: 1000,
@@ -17,13 +25,32 @@ const Profile = () => {
   return (
     <div>
       <PlayerInfo {...playerStats} />
-      <BindingForm /* 传递绑定表单所需的props */ />
+
+      <BindingForm
+        bindingCardNumber={bindingCardNumber}
+        setBindingCardNumber={setBindingCardNumber}
+        bindingBank={bindingBank}
+        setBindingBank={setBindingBank}
+        bindingExchangeCode={bindingExchangeCode}  // ✅ 传递新的 `props`
+        setBindingExchangeCode={setBindingExchangeCode}
+        bindingCardHolder={bindingCardHolder}
+        setBindingCardHolder={setBindingCardHolder}
+        onSubmit={() => { /* 绑定的提交逻辑 */ }}
+      />
+
       <ExchangeForm /* 传递兑换表单所需的props */ />
-      <button onClick={() => setShowSupportDialog(true)}>联系客服</button>
+
+      <button
+        type="button"
+        title="고객에게 연락하세요"
+        onClick={() => setShowSupportDialog(true)}
+      >
+        고객에게 연락하세요
+      </button>
 
       {showSupportDialog && <SupportDialog onClose={() => setShowSupportDialog(false)} />}
     </div>
   );
 };
-
+console.log(React.version);
 export default Profile;
